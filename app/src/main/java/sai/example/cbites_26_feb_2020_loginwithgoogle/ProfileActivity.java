422241b591a5df9ac117200cc5663082a5c0a6f5
vishalbakshi90 <vishalbakshi90@gmail.com>
2020-02-27 11:53:02 +0530

    Initial commit

---
+++ app/src/main/java/sai/example/cbites_26_feb_2020_loginwithgoogle/ProfileActivity.java
@@ -0,0 +1,56 @@
+package sai.example.cbites_26_feb_2020_loginwithgoogle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textName, textEmail;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        imageView = findViewById(R.id.imageView);
+        textName = findViewById(R.id.textViewName);
+        textEmail = findViewById(R.id.textViewEmail);
+
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        Glide.with(this)
+                .load(user.getPhotoUrl())
+                .into(imageView);
+
+        textName.setText(user.getDisplayName());
+        textEmail.setText(user.getEmail());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //if the user is not logged in
+        //opening the login activity
+        if (mAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+
+    }
+}
